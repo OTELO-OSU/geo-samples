03a9148aa469fe07ced70aceb52164a959efb6a4
anthony <ironman@liec-jarvis.liec.site.univ-lorraine.fr>
2019-01-25 16:15:04 +0100

    FIx CAS

--- Backend/src/index.php
+++ Backend/src/index.php
@@ -170,7 +170,7 @@ $app->get('/loginCAS', function (Request $req, Response $responseSlim) {
 		$_SESSION['mail']      = $checkuser->mail;
 		$_SESSION['admin']     = $checkuser->type;
 		//$id_user  = Users::select('id_user')->where('mail', '=', $checkuser->mail)->first();
-		$user->giveRight($checkuser->id_user);
+		$user->giveRight($checkuser);
 		return $responseSlim->withRedirect('accueil');
 	} else {
 		$loader = new Twig_Loader_Filesystem('geosamples/frontend/templates');
