2f73bfeb6825984a5e3afc9efed0a5e966ed272b
phscheffer <philippe.scheffer@univ-lorraine.fr>
2021-05-25 15:07:27 +0200

    correction model MailValidation et LostPassword

--- Backend/src/geosamples/model/LostPassword.php
+++ Backend/src/geosamples/model/LostPassword.php
@@ -5,6 +5,7 @@ namespace geosamples\model;
 use Illuminate\Database\Eloquent\Model as model;
 
 class LostPassword extends model{
+	public $incrementing = false;
 	protected $table = 'lost_password';
 	protected $primaryKey = 'mail';
 	private $lost_password;
--- Backend/src/geosamples/model/MailValidation.php
+++ Backend/src/geosamples/model/MailValidation.php
@@ -5,6 +5,7 @@ namespace geosamples\model;
 use Illuminate\Database\Eloquent\Model as model;
 
 class MailValidation extends model{
+	public $incrementing = false;
 	protected $table = 'mail_validation';
 	protected $primaryKey = 'mail';
 	private $mailvalidation;
